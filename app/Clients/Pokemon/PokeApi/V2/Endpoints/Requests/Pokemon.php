ec7fbc9216bed1315032a839e190cff0e3702ab2
joaol23 <joaolucas.m.ferreira2008@gmail.com>
2023-12-04 16:18:32 -0300

    feat(cache_pokemon) Fazendo cache para cada nome especifico de pokemon que é o mesmo valor

--- app/Clients/Pokemon/PokeApi/V2/Endpoints/Requests/Pokemon.php
+++ app/Clients/Pokemon/PokeApi/V2/Endpoints/Requests/Pokemon.php
@@ -4,13 +4,27 @@ namespace App\Clients\Pokemon\PokeApi\V2\Endpoints\Requests;
 
 use App\Clients\Pokemon\PokeApi\V2\Endpoints\BaseGetRequest;
 use App\Clients\Pokemon\PokeApi\V2\Entities\interfaces\EntityInterface;
-use App\Clients\Pokemon\PokeApi\V2\Entities\interfaces\EntityListInterface;
 use App\Clients\Pokemon\PokeApi\V2\Entities\Unit\PokemonEntity;
 use App\Exceptions\Clients\Requests\ParameterNotSet;
+use App\Exceptions\ObjectNotFound;
+use Illuminate\Support\Facades\Cache;
 
 class Pokemon extends BaseGetRequest
 {
     private string $pokemon;
+
+    public function get(): PokemonEntity
+    {
+        try {
+            /** @var PokemonEntity */
+            return Cache::rememberForever(
+                "pokemon_{$this->pokemon}",
+                fn() => parent::get()
+            );
+        } catch (\Throwable $e) {
+            throw new ObjectNotFound('Pokemon');
+        }
+    }
 
     public function namePokemon(string $name): static
     {
