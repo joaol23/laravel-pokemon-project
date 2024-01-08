e058643df4583bd8b9308c995f17458f9b618f02
joaol23 <joaolucas.m.ferreira2008@gmail.com>
2024-01-08 19:29:02 -0300

    tests: fazendo testes de arquitetura mais úteis

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -1,7 +1,23 @@
 <?php
 
-test('Sem debugs no projeto')
-    ->only()
+arch('Sem debugs no projeto')
     ->expect(['dd', 'dump', 'var_dump', 'die'])
     ->not->toBeUsed();
 
+arch('Enums')
+    ->expect('App\Enum')
+    ->toBeEnums();
+
+arch('Contracts')
+    ->expect('App\Contracts')
+    ->toBeInterfaces();
+
+arch('models')
+    ->todo()
+    ->expect('App\Models')
+    ->toOnlyBeUsedIn('App\Repositories')
+    ->ignoring('Database\\');
+
+arch('app')
+    ->expect('App\Http\Controllers')
+    ->toHaveSuffix('Controller');
